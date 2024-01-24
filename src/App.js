2158c1f32e2c6e71267dc18085124eea3dedfb0c
imie <88396167+imiee@users.noreply.github.com>
2024-01-24 15:55:43 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>Main content goes here.</p>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.css';
+
+function Header() {
+    return (
+        <header>
+            This is the header
+        </header>
+    );
+}
+
+export default Header;
